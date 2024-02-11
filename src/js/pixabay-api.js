573a1b795a5171c915ad89c39f9ca036ef2134af
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-02-11 01:49:19 +0100

    save

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,9 @@
 // Описаний у документації
 import iziToast from "izitoast";
-//  Додатковий імпорт стилів
- import "izitoast/dist/css/iziToast.min.css";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
 const searchForm = document.querySelector('.js-search-form');
 const getImage = document.querySelector(".gallery");
 
@@ -9,22 +11,22 @@ const getImage = document.querySelector(".gallery");
 
 searchForm.addEventListener('submit', e => {
     e.preventDefault();
-    const query = e.target.elements.search.value;   
+    const query = e.target.elements.search.value;
     getPostsByUser(query)
         .then(data => {
-    if (data.hits.length === 0) {
-        // Показати повідомлення про відсутність зображень
-        iziToast.error({
-            title: 'Error',
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-            position: 'topCenter'
-        });
-    }
-                else {
-            // Обробити отримані дані зображень
-            renderImage(data.hits);
-        }
-    })
+            if (data.hits.length === 0) {
+                // Показати повідомлення про відсутність зображень
+                iziToast.error({
+                    title: 'Error',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topCenter'
+                });
+            }
+            else {
+                // Обробити отримані дані зображень
+                renderImage(data.hits);
+            }
+        })
         .catch(error => {
             // Обробити помилку запиту
             console.error('There was a problem with the fetch operation:', error);
@@ -34,8 +36,9 @@ searchForm.addEventListener('submit', e => {
                 message: 'There was a problem with the fetch operation. Please try again later.',
                 position: 'topCenter'
             });
-    e.target.reset();
-})
+            e.target.reset();
+        });
+});
 
  
 function getPostsByUser(query) {
@@ -44,13 +47,13 @@ function getPostsByUser(query) {
     const API_KEY = '42305658-75508782eac06a666c1fb928b';
 
     const params = {
-    key: API_KEY,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    q: query
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        q: query
     
-}
+    };
 
 
     const url = `${BASE_URL}${END_POINT}?${new URLSearchParams(params)}`;
@@ -58,8 +61,8 @@ function getPostsByUser(query) {
     
 
    return fetch(url).then(res => res.json());
-
     };
+
     function displayImages(image) {
         return `<ul class='gallery'><li class="gallery-item">
     <img class="gallery-image" src="${image.previewURL}" alt="#"/></li></ul>`
@@ -68,5 +71,5 @@ function getPostsByUser(query) {
     function renderImage(data) {
         const markup = data.map(displayImages).join('');
 
-        getImage.innerHTML = `<ul class='gallery'>${markup}</ul>`;
-}
+        getImage.innerHTML = `<ul class='gallery'>${markup}</ul>`
+    }
