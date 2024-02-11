fce924a85859536dbf82eb05c5fa61c2e89e24d8
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-02-11 01:03:35 +0100

    гзвфеу

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,70 @@
 // Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
+//  Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+ 
 
 
-const listOfParams = {
-    key:'42305658-75508782eac06a666c1fb928b',
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    
-}
-function getPostsByUser(userValue) {
+const searchForm = document.querySelector('.js-search-form');
+const getImage = document.querySelector(".gallery");
+
+
+
+searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const query = e.target.elements.search.value;   
+    getPostsByUser(query)
+        .then(data => {
+    if (data.hits.length === 0) {
+        // Показати повідомлення про відсутність зображень
+        iziToast.error({
+            title: 'Error',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topCenter'
+        });
+    }
+                else {
+            // Обробити отримані дані зображень
+            renderImages(data.hits);
+        }
+    })
+        .catch(error => {
+            // Обробити помилку запиту
+            console.error('There was a problem with the fetch operation:', error);
+            // Показати повідомлення про помилку
+            iziToast.error({
+                title: 'Error',
+                message: 'There was a problem with the fetch operation. Please try again later.',
+                position: 'topCenter'
+            });
+    e.target.reset;
+})
+
+ 
+function getPostsByUser(query) {
     const BASE_URL = "https://pixabay.com";
     const END_POINT = '/api/';
-    const params = `?q=${userValue}`;
+    const API_KEY = '42305658-75508782eac06a666c1fb928b';
 
-    const url = BASE_URL + END_POINT + params;
-    const OPTIONS = {
-        headers: {
-            "X-RateLimit-Limit": 'The maximum number of requests that the consumer is permitted to make in 30 minutes.',
-        "X-RateLimit-Remaining": 'The number of requests remaining in the current rate limit window.',
-         " X-RateLimit-Reset": 'The remaining time in seconds after which the current rate limit window resets.',
-        }
-    }
+    const params = {
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        q: query
+    
+    };
+
+
+    const url = `${BASE_URL}${END_POINT}?${new URLSearchParams(params)}`;
+
+    
+
+   return fetch(url).then(res => res.json());
+
+    };
+  
+function renderImages(images) {
+    const markup = images.map(image => `<img class="gallery-image" src="${image.previewURL}" alt="#">`).join('');
+    getImage.innerHTML = markup;
 }
